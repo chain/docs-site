6f477f3d590cedbdf78056f3b2d702db7b6e24c0
zachterrell57 <zachterrell@gmail.com>
2022-07-07 13:47:03 -0500

    changing base url

--- chain-docs/docusaurus.config.js
+++ chain-docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "Chain Docs",
   tagline: "Complete documentation for the Chain Ecosystem",
   url: "https://chain.github.io",
-  baseUrl: "/",
+  baseUrl: "/docs-site/",
   trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
