e63aaf29fc9a67b8973b86ccbad03f0cc18752a0
SSardorf <simon.sardorf@chain.com>
2023-01-31 17:51:08 +0100

    Remove XCN from rest of docs site

--- src/components/HomepageOptions/index.js
+++ src/components/HomepageOptions/index.js
@@ -4,77 +4,79 @@ import styles from "./styles.module.css";
 import { Link } from "react-router-dom";
 
 const OptionList = [
-  {
-    title: "Sequence",
-    img: require("@site/static/img/sequence-full.gif").default,
-    description: (
-      <>
-        Sequence is a ledger-as-a-service that enables organizations to securely
-        track and transfer balances in a token format.
-      </>
-    ),
-  },
-  {
-    title: "XCN",
-    img: require("@site/static/img/xcn-full.gif").default,
-    description: (
-      <>
-        The Chain Ecosystem Cryptocurrency. Stake your XCN today to participate
-        in Chain Governance.
-        {/* these line breaks make the cards the same hight, should fix in CSS */}
-        {/* <br /><br /> */}
-      </>
-    ),
-  },
-  {
-    title: "Cloud",
-    img: require("@site/static/img/cloud-full.gif").default,
-    description: (
-      <>
-        Decentralized infrastructure protocol designed for developers to access
-        blockchain networks on-demand.
-        {/* these line breaks make the cards the same hight, should fix in CSS */}
-        {/* <br /><br /> */}
-      </>
-    ),
-  },
+    {
+        title: "Sequence",
+        img: require("@site/static/img/sequence-full.gif").default,
+        description: (
+            <>
+                Sequence is a ledger-as-a-service that enables organizations to
+                securely track and transfer balances in a token format.
+            </>
+        ),
+    },
+    {
+        title: "XCN",
+        img: require("@site/static/img/xcn-full.gif").default,
+        description: (
+            <>
+                The Chain Ecosystem Cryptocurrency. Stake your XCN today to
+                participate in Chain Governance.
+                {/* these line breaks make the cards the same hight, should fix in CSS */}
+                {/* <br /><br /> */}
+            </>
+        ),
+    },
+    {
+        title: "Cloud",
+        img: require("@site/static/img/cloud-full.gif").default,
+        description: (
+            <>
+                Decentralized infrastructure protocol designed for developers to
+                access blockchain networks on-demand.
+                {/* these line breaks make the cards the same hight, should fix in CSS */}
+                {/* <br /><br /> */}
+            </>
+        ),
+    },
 ];
 
 function Option({ img, title, description }) {
-  var link;
-  if (title === "Sequence") {
-    link = "/docs/sequence/get-started/introduction";
-  } else if (title === "XCN") {
-    link = "/docs/xcn/token/introduction";
-  } else if (title === "Cloud") {
-    link = "/docs/cloud/introduction";
-  } else {
-    link = "/";
-  }
-  return (
-    <div className={styles.optionCard}>
-      <Link to={link}>
-        <div className={styles.content}>
-          <div className={styles.featureGifContainer}>
-            <img src={img} className={styles.featureGif} role="img" />
-          </div>
-          <div className={styles.optionDescription}>
-            <p>{description}</p>
-          </div>
+    var link;
+    if (title === "Sequence") {
+        link = "/docs/sequence/get-started/introduction";
+    } else if (title === "Cloud") {
+        link = "/docs/cloud/introduction";
+    } else {
+        link = "/";
+    }
+    return (
+        <div className={styles.optionCard}>
+            <Link to={link}>
+                <div className={styles.content}>
+                    <div className={styles.featureGifContainer}>
+                        <img
+                            src={img}
+                            className={styles.featureGif}
+                            role="img"
+                        />
+                    </div>
+                    <div className={styles.optionDescription}>
+                        <p>{description}</p>
+                    </div>
+                </div>
+            </Link>
         </div>
-      </Link>
-    </div>
-  );
+    );
 }
 
 export default function HomepageOptions() {
-  return (
-    <section className={styles.features}>
-      <div className={styles.optionContainer}>
-        {OptionList.map((props, idx) => (
-          <Option key={idx} {...props} />
-        ))}
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className={styles.optionContainer}>
+                {OptionList.map((props, idx) => (
+                    <Option key={idx} {...props} />
+                ))}
+            </div>
+        </section>
+    );
 }
